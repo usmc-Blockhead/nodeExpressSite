3ce4209ab3ff8d20965d346972a58136ad35a195
Christa <jaysplaypen@gmail.com>
2023-03-29 13:36:05 -0700

    Express Generator

---
+++ routes/partnerRouter.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const partnerRouter = express.Router();
+
+partnerRouter.route('/')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the partner to you');
+})
+.post((req, res) => {
+    res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partner');
+})
+.delete((req, res) => {
+    res.end('Deleting all partner');
+});
+
+partnerRouter.route('/:partnerId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end('Will send all the partner to you');
+})
+.post((req, res) => {
+    res.end(`Will add the partner: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /partner');
+})
+.delete((req, res) => {
+    res.end('Deleting all partner');
+});
+module.exports = partnerRouter;
