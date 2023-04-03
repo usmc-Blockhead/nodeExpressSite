075467f770d6ed3ea5703f22174ce7bea1ebb138
Christa <jaysplaypen@gmail.com>
2023-04-02 17:52:06 -0700

    workshop2

--- routes/partnerRouter.js
+++ routes/partnerRouter.js
@@ -41,7 +41,6 @@ partnerRouter.route('/')
 partnerRouter.route('/:partnerId')
 .all((req, res, next) => {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
     next();
 })
 .get((req, res, next) => {
