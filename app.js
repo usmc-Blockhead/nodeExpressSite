dfbf14f767e1777693d52e4039e963a18272999c
Christa <jaysplaypen@gmail.com>
2023-04-11 17:54:08 -0700

    File Upload

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var usersRouter = require("./routes/users");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionRouter = require("./routes/promotionRouter");
 const partnerRouter = require("./routes/partnerRouter");
+const uploadRouter = require('./routes/uploadRouter');
+
 
 // mongoDB Server
 const mongoose = require("mongoose");
@@ -69,6 +71,7 @@ app.use("/users", usersRouter);
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionRouter);
 app.use("/partners", partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
